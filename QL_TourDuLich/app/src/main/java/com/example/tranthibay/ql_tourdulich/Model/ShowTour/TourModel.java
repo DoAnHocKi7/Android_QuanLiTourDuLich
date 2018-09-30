5373135d94baf830758a2e30d46de242095c2039
TranKhats <35250985+TranKhats@users.noreply.github.com>
2018-09-30 19:57:37 +0700

    temp_file

--- QL_TourDuLich/app/src/main/java/com/example/tranthibay/ql_tourdulich/Model/ShowTour/TourModel.java
+++ QL_TourDuLich/app/src/main/java/com/example/tranthibay/ql_tourdulich/Model/ShowTour/TourModel.java
@@ -1,16 +1,7 @@
 package com.example.tranthibay.ql_tourdulich.Model.ShowTour;
 
 public class TourModel {
-//    java.lang.String Ten;
-//    java.lang.String Ma;
-//    java.lang.String MoTa;
-//
-//    public TourModel(java.lang.String ten, java.lang.String ma, java.lang.String moTa) {
-//        Ten = ten;
-//        Ma = ma;
-//        MoTa = moTa;
-//
-//    }
+
     private String maTour;
 
     public String getMaTour() {
